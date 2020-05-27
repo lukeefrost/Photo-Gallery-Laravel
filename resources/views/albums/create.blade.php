5828fc2b0aaa21a91ddfe27ea7cd81d22a6a24b7
lukelliotfrost <luke@radio.co>
2020-05-27 17:13:33 +0100

    Added form to create a new album. Also fixed error with migration file.

---
+++ resources/views/albums/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+  <h2>Create new Album</h2>
+
+  <form method="post" action="{{ route('album-store') }}" enctype="multipart/form-data">
+    @csrf
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name" name="name" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+      <label for="description">Description</label>
+      <input type="email" class="form-control" id="description" name="description" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+      <label for="cover-image">Cover Image</label>
+      <input type="file" class="form-control" id="cover-image" name="cover-image">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Photo Gallery</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    yield('content')
+  </body>
+</html>
